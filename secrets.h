28b90a3371f735bbb238711c9fee7e7dc7478595
sighman <g00286398@atu.ie>
2025-05-02 18:48:03 +0100

    final push

--- secrets.h
+++
@@ -1,8 +0,0 @@
-// Use this file to store all of the private credentials 
-// and connection details
-
-#define SECRET_SSID "SimonPhone"		// replace MySSID with your WiFi network name
-#define SECRET_PASS "ajzm2898"	// replace MyPassword with your WiFi password
-
-#define SECRET_CH_ID 2712952					//Test channel for counting
-#define SECRET_WRITE_APIKEY "P1XBR0AGFAAEMZ7B"	//API Key for Test channel
